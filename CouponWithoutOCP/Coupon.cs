a6a9710c483720f9089cf7f0a4fadf351a46de89
ivan nur ilham syah <~@DESKTOP-RB2MITU>
2020-12-15 17:20:40 -0800

    add coupon class

---
+++ CouponWithoutOCP/Coupon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithoutOCP
+{
+    class Coupon
+    {
+        public double discNominal = 0;
+        public double priceNett (double originPrice)
+        {
+            double net = 0;
+            net = (100 - discNominal) * originPrice / 100;
+            return net;
+        }
+    }
+}
---
+++ CouponWithoutOCP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CouponWithoutOCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coupon coupon1 = new Coupon();
+            coupon1.discNominal = 2000;
+            Console.WriteLine(coupon1.priceNett(1000));
+        }
+    }
+}
